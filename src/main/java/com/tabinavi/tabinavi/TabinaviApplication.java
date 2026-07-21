90427c626ad7df8a26eed7250987aef98885da33
yum97 <141902228+yum97@users.noreply.github.com>
2026-07-21 16:18:44 +0900

    Add files via upload

---
+++ src/main/java/com/tabinavi/tabinavi/TabinaviApplication.java
@@ -0,0 +1,13 @@
+package com.tabinavi.tabinavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabinaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabinaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabinavi/tabinavi/TabinaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabinavi.tabinavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabinaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
